e598496d3ddf4f629b254eaefe752642fc927849
Gabriel-Henrique-04 <gabrielhnascimento04@gmail.com>
2024-11-05 20:40:58 -0300

    Add files via upload

---
+++ Q5-Gabriel_Henrique.py
@@ -0,0 +1,21 @@
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
+c = int(input("Digite novamente outro número: "))
+
+if a > b :
+    print("FÉ o maior valor: ", a) 
+
+if a > c :
+    print("É o maior valor: ", a)
+    
+if b > a :
+    print("É o maior valor: ", b) 
+
+if b > c :
+    print("É o maior valor: ", b) 
+
+if c > a :
+    print("É o maior valor: ", c)
+
+if c > b :
+    print("É o maior valor: ", c)
